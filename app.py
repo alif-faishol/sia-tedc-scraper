9f862ef8841127611e99a8cbb4d0a1bfbac47aba
Alif Faishol <alifaishol@gmail.com>
2018-04-04 06:16:09 +0700

    comments

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import json
 import mechanicalsoup
 from requests.cookies import RequestsCookieJar
 from bs4 import BeautifulSoup
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 
 app = Flask(__name__)
 
@@ -12,6 +11,8 @@ def hello():
 
 @app.route("/login", methods=["POST"])
 def logging_in():
+
+    # Fill login form and submit
     browser = mechanicalsoup.StatefulBrowser()
     browser.open('http://siakad.poltektedc.ac.id')
     form = browser.select_form(selector='form')
@@ -19,6 +20,7 @@ def logging_in():
     form['passwd'] = request.json['password']
     browser.submit_selected()
 
+    # Get the cookie from target website if login not failed
     page = str(browser.get_current_page())
     if '<form action="/politeknik/mahasiswa.php"' in page:
         response = make_response('{"status":"failed"}', 200)
@@ -36,18 +38,26 @@ def logging_in():
 @app.route("/data")
 def get_data():
     if "session" in request.cookies:
+
+        # Use cookie from client request, with slight edit
         cookie = ''.join(reversed(request.cookies["session"][0:10])) + request.cookies["session"][10:]
         cj = RequestsCookieJar()
         cj.set("PHPSESSID", cookie)
+
+        # Setup browser, use cookie from client
         browser = mechanicalsoup.StatefulBrowser()
         browser.set_cookiejar(cj)
-        browser.open('http://siakad.poltektedc.ac.id/politeknik/mahasiswa.php')
+
         data = {}
+
+        # Get data from mahasiswa.php page
+        browser.open('http://siakad.poltektedc.ac.id/politeknik/mahasiswa.php')
         data["bio"] = {}
         data["bio"]["nama"] = browser.get_current_page().select('td[colspan] table[align] td strong')[0].text
         data["bio"]["jurusan"] = browser.get_current_page().select('td[colspan] table[align] td strong')[1].text
-        data["bio"]["angkatan"] = browser.get_current_page().select('td[colspan] table[align] td strong')[2].text
-        return str(data)
+        data["bio"]["angkatan"] = browser.get_current_page().select('td[colspan] table[align] td strong')[3].text
+
+        return jsonify(data)
     else:
         return 'tydac'
 
