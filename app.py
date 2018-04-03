e18f1b0a63b61d29b2011c4476601338393d39ce
Alif Faishol <alifaishol@gmail.com>
2018-04-03 20:16:41 +0700

    login success/failed status

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import json
-import base64
 import mechanicalsoup
 import http.cookiejar
 from bs4 import BeautifulSoup
 from flask import Flask, request, make_response
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
@@ -16,17 +15,27 @@ def logging_in():
     browser = mechanicalsoup.StatefulBrowser()
     browser.open('http://siakad.poltektedc.ac.id')
     form = browser.select_form(selector='form')
-    #form['username'] = request.data['username']
-    #form['passwd'] = request.data['password']
+    form['username'] = request.json['username']
+    form['passwd'] = request.json['password']
+    browser.submit_selected()
 
-    cj = browser.get_cookiejar()
-    cookie = ""
-    for c in cj:
-        cookie = c.value
+    page = str(browser.get_current_page())
+    if '<form action="/politeknik/mahasiswa.php"' in page:
+        response = make_response('{"status":"failed"}', 200)
+    else:
+        cj = browser.get_cookiejar()
+        cookie = ""
+        for c in cj:
+            cookie = c.value
 
-    response = make_response('{"status":"success"}', 200)
-    response.set_cookie('session', value=''.join(reversed(cookie[0:10])) + cookie[10:])
+        response = make_response('{"status":"success"}', 200)
+        response.set_cookie('session', value=''.join(reversed(cookie[0:10])) + cookie[10:])
+
     return response
+
+def get_data():
+    return ''
+
 
 if __name__=='__main__':
     app.run()
